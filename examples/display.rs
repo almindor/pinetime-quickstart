74ce17251b268b43fbfd1e545fee8c299ac98128
Ales Katona <almindor@gmail.com>
2020-02-16 20:01:44 -0700

    finish display example

--- examples/display.rs
+++ examples/display.rs
@@ -7,14 +7,14 @@ extern crate nrf52832_hal;
 extern crate panic_halt;
 
 use cortex_m_rt::entry;
-use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::Circle;
+use embedded_graphics::primitives::*;
 use nrf52832_hal::gpio::Level;
 use nrf52832_hal::gpio::*;
 use nrf52832_hal::spim;
 use nrf52832_hal::Delay;
-use st7735_lcd::ST7735;
+use st7735_lcd::{ST7735, Orientation};
+use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
@@ -27,10 +27,10 @@ fn main() -> ! {
     let _backlight = port0.p0_22.into_push_pull_output(Level::Low); // set medium backlight on
     let rst = port0.p0_26.into_push_pull_output(Level::Low); // reset pin
     let _cs = port0.p0_25.into_push_pull_output(Level::Low); // keep low while drivign display
-    let cd = port0.p0_18.into_push_pull_output(Level::Low); // clock/data switch
+    let dc = port0.p0_18.into_push_pull_output(Level::Low); // data/clock switch
 
-    let spiclk = port0.p0_02.into_push_pull_output(Level::Low).degrade();
-    let spimosi = port0.p0_03.into_push_pull_output(Level::Low).degrade();
+    let spiclk = port0.p0_02.into_push_pull_output(Level::Low).degrade(); // SPI clock to LCD
+    let spimosi = port0.p0_03.into_push_pull_output(Level::Low).degrade(); // SPI MOSI to LCD
 
     let pins = spim::Pins {
         sck: spiclk,
@@ -38,14 +38,33 @@ fn main() -> ! {
         mosi: Some(spimosi),
     };
 
+    // create SPI interface
     let spi = spim::Spim::new(p.SPIM0, pins, spim::Frequency::M8, spim::MODE_3, 122);
 
-    let mut display = ST7735::new(spi, cd, rst, true, false);
-    let rgb = (128, 128, 128);
+    // create driver
+    let mut display = ST7735::new(spi, dc, rst, true, true);
 
+    // initialize
     display.init(&mut delay).unwrap();
+    // set default orientation
+    display.set_orientation(&Orientation::Portrait).unwrap();
 
-    display.draw(Circle::<Rgb565>::new(Coord::new(64, 64), 64).stroke(Some(rgb.into())));
+    let green = (0, 255, 0);
+    let blue = (0, 0, 255);
+    let red = (255, 0, 0);
+    
+    let blank = Rectangle::new(Coord::new(0, 0), Coord::new(239, 239)).fill(Some(blue.into()));
+    let circle1 = Circle::new(Coord::new(128, 64), 64).fill(Some(red.into()));
+    let circle2 = Circle::new(Coord::new(64, 64), 64).stroke(Some(green.into()));
 
-    loop {}
+    // draw two circles on blue background
+    display.draw(blank);
+    display.draw(circle1);
+    display.draw(circle2);
+
+    hprintln!("Rendering done").unwrap();
+
+    loop {
+        continue; // keep optimizer from removing in --release
+    }
 }
