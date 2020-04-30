684e8e2f9b1cb03282b23364caa21b90367b898f
Ales Katona <ales@katona.me>
2020-04-30 10:08:13 -0600

    use latest dependencies

--- examples/battery_status.rs
+++ examples/battery_status.rs
@@ -11,19 +11,19 @@ use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 use embedded_hal::adc::OneShot;
 use embedded_hal::digital::v2::InputPin;
-use nrf52832_hal::gpio::*;
+use nrf52832_hal::gpio::p0::Parts;
 use nrf52832_hal::prelude::_embedded_hal_blocking_delay_DelayMs;
 use nrf52832_hal::saadc::*;
 use nrf52832_hal::Delay;
 
 #[entry]
 fn main() -> ! {
-    let core = nrf52832_hal::nrf52832_pac::CorePeripherals::take().unwrap();
+    let core = nrf52832_hal::pac::CorePeripherals::take().unwrap();
     let mut delay = Delay::new(core.SYST);
 
-    let p = nrf52832_hal::nrf52832_pac::Peripherals::take().unwrap();
+    let p = nrf52832_hal::pac::Peripherals::take().unwrap();
     let mut saadc = Saadc::new(p.SAADC, SaadcConfig::default());
-    let port0 = p.P0.split();
+    let port0 = Parts::new(p.P0);
 
     let cip = port0.p0_12.into_floating_input().degrade(); // charge indication pin
     let mut adc = port0.p0_31.into_floating_input(); // adc voltage analog input pin
